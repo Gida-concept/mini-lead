02bee65b057c665c61e3a7649af3e7b4be138f70
Healer Jay <gidatechnologies@gmail.com>
2026-06-15 15:21:26 +0100

    healer

--- server/src/services/queryBuilder.ts
+++ server/src/services/queryBuilder.ts
@@ -12,7 +12,7 @@ export function buildInstagramQuery(businessType: string, location: string): str
 }
 
 export function buildGoogleWebQuery(businessType: string, location: string): string {
-  return `"${businessType}" "${location}" "menu" OR "reservation" OR "order" -site:facebook.com -site:instagram.com -site:tripadvisor.com -site:eatdrinklagos.com -site:booking.com -site:yellowpages.com.ng -site:ng.linkedin.com`;
+  return `intitle:"${businessType}" "${businessType}" "${location}" ("contact" OR "about" OR "official" OR "location") -"Top 10" -"Top 5" -site:tripadvisor.com -site:booking.com -site:expedia.com -site:hotels.com -site:kayak.com -site:trivago.com -site:yelp.com -site:yellowpages.com -site:superpages.com -site:manta.com -site:hotfrog.com -site:cylex.us.com -site:citysearch.com -site:foursquare.com -site:justdial.com -site:bbb.org -site:wikimapia.org -site:facebook.com -site:instagram.com -site:linkedin.com -inurl:top-10 -inurl:top10 -inurl:blog -inurl:category -inurl:tag`;
 }
 
 export function buildGoogleMapsSearchString(businessType: string, location: string): string {
