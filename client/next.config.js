da7baab6f82c26569e2eb9c15fd4994f0d80e52f
Healer Jay <gidatechnologies@gmail.com>
2026-06-16 18:36:26 +0100

    healer

--- client/next.config.js
+++ client/next.config.js
@@ -1,14 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://localhost:3001/api/:path*",
-      },
-    ];
-  },
+  ...(process.env.NODE_ENV === 'development' && {
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'http://localhost:3001/api/:path*',
+        },
+      ];
+    },
+  }),
 };
 
 export default nextConfig;
